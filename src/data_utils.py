7c55e59541846e2a9964fef2c4829674b5f5977f
Vaishnavi Singhal <vaishnavisinghal25@gmail.com>
2026-01-18 01:23:40 +0530

    Created 3 functions- one to make sure the DNA sequence contains only ACGT(nitrogenous bases in DNA), a second one to count the number of each base, and the third one to find GC %

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,1 +1,19 @@
-
+def is_valid_dna(sequence):
+    sequence = sequence.upper()
+    for base in sequence:
+        if base not in 'ACGT':
+            return False
+    return True
+def count_bases(sequence):
+    sequence = sequence.upper()
+    counts={'A': 0, 'C': 0, 'G': 0, 'T': 0}
+    for base in sequence:
+        if base in counts:
+            counts[base] += 1
+    return counts
+def gc_content(base_counts):
+    gc_count = base_counts['G'] + base_counts['C']
+    total_count = sum(base_counts.values())
+    if total_count == 0:
+        return 0.0
+    return (gc_count / total_count) * 100
